6a1589b4f928bdfaf0258edead86239fd937ca6d
KevinYu1580 <kevin1580@gmail.com>
2022-10-27 00:19:57 +0800

    2

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ import navbar from '@/components/navbar.vue'
     <router-link to="/about">About</router-link>
     <router-link to="/todo">todo</router-link> -->
 
-    <router-view />
+    <router-view/>
     
 </template>
 
